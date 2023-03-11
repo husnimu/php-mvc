d0f0deeb9cf0a62b224b07aa0c5fc106908ba3ce
husni mubarok <husni060300@gmail.com>
2023-03-11 11:46:04 +0700

    routing

--- app/core/App.php
+++ app/core/App.php
@@ -7,6 +7,34 @@ class App
 
   public function __construct()
   {
-    echo 'App is running';
+    // var_dump($_SERVER['REQUEST_URI']);
+    $url = $this->parseUrl();
+    var_dump($url);
+  }
+
+  public function parseUrl()
+  {
+    // get request url and remove the '/' from the right
+    $url = rtrim($_SERVER['REQUEST_URI'], '/');
+    $url = filter_var($url, FILTER_SANITIZE_URL);
+
+    // check if method is get
+    if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+      // check if url is empty
+      if (empty($url)) {
+        // set url to home
+        $url = 'home';
+      }
+
+      // check if url has a query string
+      if (strpos($url, '?') !== false) {
+        // explode query string from url
+        $url = explode('?', $url);
+        // set url to first element of array
+        $url = $url[0];
+      }
+    }
+
+    return $url;
   }
 }
