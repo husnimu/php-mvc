fcff23fd9461b8acbf1ae5f6c61307cc3cae8fef
husni mubarok <husni060300@gmail.com>
2023-03-11 14:22:59 +0700

    controller

--- app/core/App.php
+++ app/core/App.php
@@ -2,6 +2,8 @@
 
 namespace App\Core;
 
+use Error;
+
 class App
 {
   protected $controller = 'Home';
@@ -10,9 +12,30 @@ class App
 
   public function __construct()
   {
-    // var_dump($_SERVER['REQUEST_URI']);
     $url = $this->parseUrl();
-    var_dump($url);
+
+    if (file_exists('../app/controllers/' . ucfirst($url[0]) . '.php')) {
+      $this->controller = ucfirst($url[0]);
+      unset($url[0]);
+    }
+
+    // require controller
+    require_once '../app/controllers/' . $this->controller . '.php';
+    $this->controller = new $this->controller;
+
+    // check if method exists
+    if (isset($url[1])) {
+      if (method_exists($this->controller, $url[1])) {
+        $this->method = $url[1];
+        unset($url[1]);
+      }
+    }
+
+    // set params to url
+    $this->params = $url ? array_values($url) : [];
+
+    // call controller method with params
+    call_user_func_array([$this->controller, $this->method], $this->params);
   }
 
   public function parseUrl()
